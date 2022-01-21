22e2dc575d97a9b166268107f098e9d36b77ee5d
GameVasionYT <bradsnewphone2006@gmail.com>
2022-01-21 17:58:31 -0500

    Add shop prices to be actually good and change with more buys

--- src/love/main.lua
+++ src/love/main.lua
@@ -5,6 +5,10 @@ function love.load()
     clicks = 0
     CHPS = 0
     clickUpgrade = 0
+    shop1Price = 10
+    shop1Owned = 0
+    shop2Price = 50
+    shop2Owned = 0
     love.window.setMode(1280, 720, {resizable=true, vsync=true, minwidth=640, minheight=360})
     -- load libriries 
     baton = require "lib.baton"
@@ -43,21 +47,53 @@ function love.keypressed(key)
 
 		love.graphics.captureScreenshot("screenshots/" .. os.time() .. ".png")
     end
+
     if key == "escape" then
         love.event.quit()
     elseif key == "1" then
-        if clicks >= 10 then
-            clicks = clicks - 10
-            CHPS = CHPS + 1
+        if shop1Owned >= 1 then
+            if clicks >= shop1Price * (shop1Owned * 1.1) then
+                price = shop1Price * (shop1Owned * 1.1)
+                shop1Owned = shop1Owned + 1
+                clicks = clicks - price
+                CHPS = CHPS + 1
+            end
+        else
+            if clicks >= shop1Price then
+                shop1Owned = shop1Owned + 1
+                clicks = clicks - shop1Owned
+                CHPS = CHPS + 1
+            end
         end
     elseif key == "2" then
-        if clicks >= 50 then
-            clicks = clicks - 50
-            clickUpgrade = clickUpgrade + 1
+        if shop2Owned >= 1 then
+            if clicks >= shop2Price * (shop2Owned * 1.1) then
+                price = shop2Price * (shop2Owned * 1.1)
+                shop2Owned = shop2Owned + 1
+                clicks = clicks - price
+                clickUpgrade = clickUpgrade + 1
+            end
+        else
+            if clicks >= shop2Price then
+                shop2Owned = shop2Owned + 1
+                clicks = clicks - shop2Price
+                clickUpgrade = clickUpgrade + 1
+            end
         end
     end
 end
 
 function love.draw()
     love.graphics.print("Clicks: " .. clicks)
+    if shop1Owned >= 1 then
+        love.graphics.print("\nShop1 price: " .. shop1Price * (shop1Owned * 1.1)) -- I hate math
+    else
+        love.graphics.print("\nShop1 price: " .. shop1Price)
+    end
+
+    if shop2Owned >= 1 then
+        love.graphics.print("\n\nShop2 price (Clicker power): " .. shop2Price * (shop2Owned * 1.1)) 
+    else
+        love.graphics.print("\n\nShop2 price (Clicker power): " .. shop2Price) 
+    end
 end
