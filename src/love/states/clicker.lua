216921edb7c22c3bb633c395645010f2e694667b
GameVasionYT <bradsnewphone2006@gmail.com>
2022-01-23 15:05:47 -0500

    controller support (hopefully)

--- src/love/states/clicker.lua
+++ src/love/states/clicker.lua
@@ -6,10 +6,12 @@ end
 
 function clicker.update(dt)
     if input:pressed("gameClick") then
-        if mouseX >= 424 and mouseX <= 893 then
-            if mouseY >= 236 and mouseY <= 466 then
+        if input:getActiveDevice() ~= "joy" then
+            if mouseX >= 424 and mouseX <= 893 and mouseY >= 236 and mouseY <= 466 then
                 clicks = clicks + 1 + clickUpgrade
             end
+        else
+            clicks = clicks + 1 + clickUpgrade
         end
     end
 end
