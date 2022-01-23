ce2acfe057dd016c8851abc16a2d5b1df232b565
GameVasionYT <bradsnewphone2006@gmail.com>
2022-01-23 15:44:37 -0500

    Fix controller support and add shopButton bind

--- src/love/settings/input.lua
+++ src/love/settings/input.lua
@@ -2,5 +2,7 @@ local gameClick
 return baton.new {
     controls = {
         gameClick = {"mouse:1", "button:a"},
-    }
+        shopButton = {"key:s", "button:back"},
+    },
+    joystick = love.joystick.getJoysticks()[1]
 }
