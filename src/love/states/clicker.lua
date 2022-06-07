1d2b2745b743c25e962f7b7ef669562ee2d0b377
GuglioIsStupid <bradsbluetab@gmail.com>
2022-06-07 12:56:09 -0400

    fix the spin spam glitch

--- src/love/states/clicker.lua
+++ src/love/states/clicker.lua
@@ -10,17 +10,21 @@ return {
             Gamestate.switch(shopMenu)
         end
         if input:pressed("speen") then
-            Timer.tween(
-                3,
-                clothingHanger,
-                {
-                    orientation = 6.27
-                },
-                "out-elastic",
-                function()
-                    clothingHanger.orientation = 0
-                end
-            )
+            if not tweeningHangerO then
+                tweeningHangerO = true
+                Timer.tween(
+                    3,
+                    clothingHanger,
+                    {
+                        orientation = 6.27
+                    },
+                    "out-elastic",
+                    function()
+                        tweeningHangerO = false
+                        clothingHanger.orientation = 0
+                    end
+                )
+            end
         end
     end,
 
